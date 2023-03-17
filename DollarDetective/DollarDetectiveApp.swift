c32c8acee2471f32a9c7e2ac18438c987268a27e
DmytroHorodyskyi <107617154+DmytroHorodyskyi@users.noreply.github.com>
2023-03-17 22:42:12 +0200

    Initial Commit

---
+++ DollarDetective/DollarDetectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarDetectiveApp.swift
+//  DollarDetective
+//
+//  Created by Dmytro Horodyskyi on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DollarDetectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
